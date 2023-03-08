10a35c2c9477328b8b89b5e982b3919d8fae3fe7
godiale <11892279+godiale@users.noreply.github.com>
2023-03-08 11:23:10 +0100

    Verbs simulator for given prefix

--- simulator.py
+++ simulator.py
@@ -1,5 +1,31 @@
+from pandas_ods_reader import read_ods
+import os
+
+WORDS_STORE = "C:/Users/godiale/Dropbox/Deutsch/Deutsche_Worter.ods"
+
+
+def clear_viewport():
+    os.system('cls')
+
+
+def read_verbs():
+    df = read_ods(WORDS_STORE, "Verben", headers=False,
+                  columns=['a', 'verb', 'b', 'translation'])\
+        .drop(columns=['a', 'b'])
+    return df[df.translation.notnull()]
+
+
 def main():
-    pass
+    df = read_verbs()
+
+    prefix = input("Enter prefix verbs: ")
+
+    for index, row in df.iterrows():
+        if not prefix or row.verb.startswith(prefix):
+            clear_viewport()
+            input(f"{row.verb} ?")
+            print(f"    {row.translation}")
+            input()
 
 
 if __name__ == '__main__':

b7ef357424932bf958a077d8d47ebc9b04e71701
godiale <11892279+godiale@users.noreply.github.com>
2023-03-08 23:45:07 +0100

    Grouping of verbs without prefixes

--- simulator.py
+++ simulator.py
@@ -1,21 +1,52 @@
 from pandas_ods_reader import read_ods
+from collections import defaultdict
+import random
 
 WORDS_STORE = "C:/Users/godiale/Dropbox/Deutsch/Deutsche_Worter.ods"
-SMALLEST_EXERCISE = 20
+SMALLEST_EXERCISE_SIZE = 20
+
+PREFIXES = [
+    'ab', 'an', 'auf', 'aus', 'be', 'ein', 'ent', 'er',
+    'ge', 'him', 'über', 'unter', 'ver', 'vor', 'zu'
+]
 
 
 def read_verbs_from_file():
     df = read_ods(WORDS_STORE, "Verben", headers=False,
                   columns=['a', 'verb', 'b', 'translation'])\
         .drop(columns=['a', 'b'])
-    return df[df.translation.notnull()]
+    df = df[df.translation.notnull()]
+    df.verb = df.verb.str.strip()
+    df.drop_duplicates(subset='verb')
+    df = df.set_index('verb', drop=False)
+    return df
 
 
 def create_exercise(df):
-    df = df.sample(frac=1).reset_index(drop=True)  # random order
-    regex = input("Enter words regex: ")
-    df = df[df.verb.str.contains(regex)]
-    if len(df.index) > SMALLEST_EXERCISE:
+    mode = input("Enter mode (plain|group) [plain]: ")
+    if mode == '':
+        mode = 'plain'
+
+    if mode == 'group':
+        r2v = defaultdict(list)
+        for v in df.verb.tolist():
+            r2v[v].append(v)
+            for p in PREFIXES:
+                if v.startswith(p):
+                    r2v[v.removeprefix(p)].append(v)
+        roots = list(r2v.keys())
+        random.shuffle(roots)
+        verbs = []
+        for r in roots:
+            if len(r2v[r]) > 1:
+                verbs.extend(r2v[r])
+        df = df.loc[verbs]
+    else:  # plain
+        df = df.sample(frac=1).reset_index(drop=True)  # random order
+        regex = input("Enter words regex: ")
+        df = df[df.verb.str.contains(regex)]
+
+    if len(df.index) > SMALLEST_EXERCISE_SIZE:
         exercise_size = input(f"Enter size of exercise: (1-{len(df.index)}): ")
         df = df.head(int(exercise_size))
     return df
